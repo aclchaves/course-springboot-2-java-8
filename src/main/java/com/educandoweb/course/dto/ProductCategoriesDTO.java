fe1dbb63e9731c91a30bc1c6aa8b0b9a9921bfba
Andre Chaves <aclchaves@gmail.com>
2019-09-21 14:51:35 -0300

    Product insert

--- src/main/java/com/educandoweb/course/dto/ProductCategoriesDTO.java
+++ src/main/java/com/educandoweb/course/dto/ProductCategoriesDTO.java
@@ -1,43 +1,35 @@
 package com.educandoweb.course.dto;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import com.educandoweb.course.entities.Product;
 
-public class ProductDTO implements Serializable{
+public class ProductCategoriesDTO implements Serializable{	
 	private static final long serialVersionUID = 1L;
 	
-	private Long id;
 	private String name;
 	private String description;
 	private Double price;
 	private String imgUrl;
 	
-	public ProductDTO() {}
+	private List<CategoryDTO> categories = new ArrayList<>();
+	
+	public ProductCategoriesDTO() {}
 
-	public ProductDTO(Long id, String name, String description, Double price, String imgUrl) {
-		super();
-		this.id = id;
+	public ProductCategoriesDTO(String name, String description, Double price, String imgUrl) {		
 		this.name = name;
 		this.description = description;
 		this.price = price;
-		this.imgUrl = imgUrl;
+		this.imgUrl = imgUrl;		
 	}
 	
-	public ProductDTO( Product entity) {
-		setId(entity.getId());
+	public ProductCategoriesDTO(Product entity) {	
 		setName(entity.getNome());
 		setDescription(entity.getDescription());
 		setPrice(entity.getPrice());
 		setImgUrl(entity.getImgUrl());
-	}
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
 	}
 
 	public String getName() {
@@ -72,9 +64,12 @@ public class ProductDTO implements Serializable{
 		this.imgUrl = imgUrl;
 	}
 
-	public Product toEntity() {
-		return new Product(id, name, description, price, imgUrl);
+	public List<CategoryDTO> getCategories() {
+		return categories;
 	}
 	
-
+	public Product toEntity() {
+		return new Product(null, name, description, price, imgUrl);
+	}
+	
 }
--- src/main/java/com/educandoweb/course/services/ProductService.java
+++ src/main/java/com/educandoweb/course/services/ProductService.java
@@ -6,10 +6,16 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.educandoweb.course.Repositories.CategoryRepository;
 import com.educandoweb.course.Repositories.ProductRepository;
+import com.educandoweb.course.dto.CategoryDTO;
+import com.educandoweb.course.dto.ProductCategoriesDTO;
 import com.educandoweb.course.dto.ProductDTO;
+import com.educandoweb.course.entities.Category;
 import com.educandoweb.course.entities.Product;
+import com.educandoweb.course.resources.CategoryResource;
 import com.educandoweb.course.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -17,6 +23,9 @@ public class ProductService {
 
 	@Autowired
 	private ProductRepository repository;
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
 	
 	public List<ProductDTO> findAll(){
 		List<Product> list = repository.findAll();
@@ -27,5 +36,22 @@ public class ProductService {
 		Optional<Product> obj = repository.findById(id);
 		Product entity = obj.orElseThrow(() -> new ResourceNotFoundException(id));
 		return new ProductDTO(entity);
+	}
+	
+	@Transactional
+	public ProductDTO insert (ProductCategoriesDTO dto) {
+		Product entity = dto.toEntity();
+		setProductCategories(entity, dto.getCategories());
+		entity = repository.save(entity);
+		return new ProductDTO(entity);
+	}
+
+	private void setProductCategories(Product entity, List<CategoryDTO> categories) {
+		entity.getCategories().clear();
+		for (CategoryDTO dto : categories) {
+			Category category = categoryRepository.getOne(dto.getId());
+			entity.getCategories().add(category);
+		}
+		
 	}
 }
