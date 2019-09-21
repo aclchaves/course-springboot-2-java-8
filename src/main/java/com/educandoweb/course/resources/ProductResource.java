bf715fe2936741326f37a44d669ee5397d7e2d4c
Andre Chaves <aclchaves@gmail.com>
2019-09-21 14:09:04 -0300

    OrderDTO

--- src/main/java/com/educandoweb/course/resources/ProductResource.java
+++ src/main/java/com/educandoweb/course/resources/ProductResource.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.educandoweb.course.dto.ProductDTO;
-import com.educandoweb.course.entities.Product;
 import com.educandoweb.course.services.ProductService;
 
 @RestController
