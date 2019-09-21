90fbc0282ff077ac4a7c5b058e8f960c308b138e
Andre Chaves <aclchaves@gmail.com>
2019-09-21 15:06:59 -0300

    Product update

--- src/main/java/com/educandoweb/course/services/ProductService.java
+++ src/main/java/com/educandoweb/course/services/ProductService.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,7 +17,6 @@ import com.educandoweb.course.dto.ProductCategoriesDTO;
 import com.educandoweb.course.dto.ProductDTO;
 import com.educandoweb.course.entities.Category;
 import com.educandoweb.course.entities.Product;
-import com.educandoweb.course.resources.CategoryResource;
 import com.educandoweb.course.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -44,8 +45,32 @@ public class ProductService {
 		setProductCategories(entity, dto.getCategories());
 		entity = repository.save(entity);
 		return new ProductDTO(entity);
+	}	
+	
+	@Transactional
+	public ProductDTO update(Long id, ProductCategoriesDTO dto) {
+		try {
+			Product entity = repository.getOne(id);
+			updateData(entity, dto);
+			entity = repository.save(entity);
+			return new ProductDTO(entity);
+		}catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
+		
 	}
 
+	private void updateData(Product entity, ProductCategoriesDTO dto) {
+		entity.setNome(dto.getName());
+		entity.setDescription(dto.getDescription());
+		entity.setPrice(dto.getPrice());
+		entity.setImgUrl(dto.getImgUrl());
+		if(dto.getCategories() != null && dto.getCategories().size() > 0){
+			setProductCategories(entity, dto.getCategories());
+			
+		}
+	}
+	
 	private void setProductCategories(Product entity, List<CategoryDTO> categories) {
 		entity.getCategories().clear();
 		for (CategoryDTO dto : categories) {
