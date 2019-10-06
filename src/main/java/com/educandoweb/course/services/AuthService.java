95ca648de402e2e301c8f47100d10371ded7f574
Andre Chaves <aclchaves@gmail.com>
2019-10-05 21:44:15 -0300

    Forgot password, Logger

--- src/main/java/com/educandoweb/course/services/AuthService.java
+++ src/main/java/com/educandoweb/course/services/AuthService.java
@@ -1,11 +1,16 @@
 package com.educandoweb.course.services;
 
+import java.util.Random;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,9 +22,12 @@ import com.educandoweb.course.entities.User;
 import com.educandoweb.course.security.JWTUtil;
 import com.educandoweb.course.services.exceptions.JWTAuthenticationException;
 import com.educandoweb.course.services.exceptions.JWTAuthorizationException;
+import com.educandoweb.course.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class AuthService {
+	
+	private static final Logger LOG = LoggerFactory.getLogger(AuthService.class);
 	
 	@Autowired
 	private AuthenticationManager authenticationManager;
@@ -29,6 +37,9 @@ public class AuthService {
 	
 	@Autowired
 	private UserRepository userRepository;
+	
+	@Autowired
+	private BCryptPasswordEncoder passwordEncoder;
 	
 	@Transactional(readOnly = true)
 	public TokenDTO authenticate(CredentialsDTO dto) {
@@ -70,6 +81,41 @@ public class AuthService {
 	public TokenDTO refreshToken() {
 		User user = authenticated();
 		return new TokenDTO(user.getEmail(), jwtUtil.generateToken(user.getEmail()));
+	}
+	
+	@Transactional
+	public void sendNewPassword(String email) {
+		User user = userRepository.findByEmail(email);
+		if(user == null) {
+			throw new ResourceNotFoundException("Email not found");
+		}
+		
+		String newPass = newPassword();
+		user.setPassword(passwordEncoder.encode(newPass));
+		
+		userRepository.save(user);
+		LOG.info("New password: " + newPass);
+		
+	}
+	
+	private String newPassword() {
+		char[] vect = new char[10];
+		for (int i = 0; i<10; i++) {
+			vect[i] = randonChar();
+		}
+		return new String(vect);
+	}
+
+	private char randonChar() {
+		Random rand = new Random();
+		int opt = rand.nextInt(3);
+		if(opt == 0) { // generate digit
+			return (char) (rand.nextInt(10) + 48);
+		}else if(opt == 1){ //generate uppercase letter
+			return (char) (rand.nextInt(26) + 65);
+		}else { // generate lowercase letter
+			return (char) (rand.nextInt(26) + 97);
+		}
 	}
 
 }
