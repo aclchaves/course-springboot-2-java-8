eb26c3d104b47392920253503b0d76c820c20d2f
Andre Chaves <aclchaves@gmail.com>
2019-10-10 17:49:29 -0300

    Product detailed search PART 2

--- src/main/java/com/educandoweb/course/services/ProductService.java
+++ src/main/java/com/educandoweb/course/services/ProductService.java
@@ -1,7 +1,9 @@
 package com.educandoweb.course.services;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import javax.persistence.EntityNotFoundException;
 
@@ -21,6 +23,7 @@ import com.educandoweb.course.dto.ProductDTO;
 import com.educandoweb.course.entities.Category;
 import com.educandoweb.course.entities.Product;
 import com.educandoweb.course.services.exceptions.DatabaseException;
+import com.educandoweb.course.services.exceptions.ParamFormatException;
 import com.educandoweb.course.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -32,9 +35,30 @@ public class ProductService {
 	@Autowired
 	private CategoryRepository categoryRepository;
 
-	public Page<ProductDTO> findAllPaged(String name, Pageable pageable) {
-		Page<Product> list = repository.findByNameContainingIgnoreCaseAndCategoriesIn(name, pageable);
+	public Page<ProductDTO> findBNameCategoryPaged(String name, String categoriesStr, Pageable pageable) {
+		Page<Product> list;
+		if(categoriesStr.equals("")){
+			list = repository.findByNameContainingIgnoreCase(name, pageable);			
+		}else {
+			List<Long> ids = paseIds(categoriesStr);
+			List<Category> categories = ids.stream().map(id -> categoryRepository.getOne(id)).collect(Collectors.toList());
+			list = repository.findByNameContainingIgnoreCaseAndCategoriesIn(name, categories, pageable);			
+		}
 		return list.map(e -> new ProductDTO(e));
+	}
+
+	private List<Long> paseIds(String categoriesStr) {
+		String[] idsArray = categoriesStr.split(",");
+		List<Long> list = new ArrayList<>();
+		for (String idStr : idsArray) {
+			try {
+				list.add(Long.parseLong(idStr));
+			} catch (NumberFormatException e) {
+				throw new ParamFormatException("Invalid categories format");
+			}
+		}
+		return list;
+
 	}
 
 	public ProductDTO findById(Long id) {
@@ -101,23 +125,23 @@ public class ProductService {
 		Page<Product> products = repository.findByCategory(category, pageable);
 		return products.map(e -> new ProductDTO(e));
 	}
-	
+
 	@Transactional
 	public void addCategory(Long id, CategoryDTO dto) {
 		Product product = repository.getOne(id);
 		Category category = categoryRepository.getOne(dto.getId());
 		product.getCategories().add(category);
-		repository.save(product);		
+		repository.save(product);
 	}
-	
+
 	@Transactional
 	public void removeCategory(Long id, CategoryDTO dto) {
 		Product product = repository.getOne(id);
 		Category category = categoryRepository.getOne(dto.getId());
 		product.getCategories().remove(category);
-		repository.save(product);		
+		repository.save(product);
 	}
-	
+
 	@Transactional
 	public void setCategories(Long id, List<CategoryDTO> dto) {
 		Product product = repository.getOne(id);
